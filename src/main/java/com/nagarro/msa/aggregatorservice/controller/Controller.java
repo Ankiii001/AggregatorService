7893ead9704553d14a3eea69bd3e911c9c85ca0c
Ankit Panwar <ankit.panwar@nagarro.com>
2020-04-12 19:58:17 +0530

    Second update

--- src/main/java/com/nagarro/msa/aggregatorservice/controller/Controller.java
+++ src/main/java/com/nagarro/msa/aggregatorservice/controller/Controller.java
@@ -2,6 +2,8 @@ package com.nagarro.msa.aggregatorservice.controller;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +13,26 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping(value = "/orderdetails")
 public class Controller {
-	public static final String userServiceUrl = System.getenv("USER_URL");
-	public static final String ordersServiceUrl = System.getenv("ORDERS_URL");
+
+	@Autowired
+	private RestTemplate request;
+	
+	@Value("${USER_URL}")
+	private String userServiceUrl;
+	@Value("${ORDERS_URL}")
+	private String ordersServiceUrl;
 
 	@GetMapping("/{id}")
 	public HashMap<String, Object> retrieveOrdersDetails(@PathVariable Integer id) {
-		System.out.println(userServiceUrl);
-		System.out.println(ordersServiceUrl);
-		RestTemplate request = new RestTemplate();
-		Object userDetails = request.getForObject(userServiceUrl + id, Object.class);
 		HashMap<String, Object> map = new HashMap<>();
+		Object userDetails;
+		try {
+		userDetails = request.getForObject(userServiceUrl + id, Object.class);
 		map.put("userDetails", userDetails);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			map.put("userDetails", "User not found at id- " + id);
+		}
 		Object orders = request.getForObject(ordersServiceUrl + id, Object.class);
 		map.put("orders", orders);
 		return map;
